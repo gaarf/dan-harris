2f392794b7f85d5d659c7a0377e1ce7faf2257dc
Adrien <gaarf@users.noreply.github.com>
2023-05-11 16:24:54 -0700

    animate cash value

--- src/lib/Craps/Cash.tsx
+++ src/lib/Craps/Cash.tsx
@@ -1,6 +1,10 @@
 import { sum } from '$lib/utils';
-import { useContext } from 'react';
+import { useContext, useEffect, useState, useMemo, useRef } from 'react';
 import { GameContext } from './game';
+
+function easeInSine(x: number): number {
+	return 1 - Math.cos((x * Math.PI) / 2);
+}
 
 export function Cash() {
 	function startOver() {
@@ -9,12 +13,34 @@ export function Cash() {
 	}
 
 	const { table, cash } = useContext(GameContext);
+	const total = useMemo(() => sum(cash, table), [cash, table]);
+
+	const [amount, setAmount] = useState(0);
+	const raf = useRef(0);
+
+	useEffect(() => {
+		cancelAnimationFrame(raf.current);
+		const ms = 2000; // anim time
+		const start = performance.now();
+		const loop = () =>
+			setAmount((a) => {
+				const r = (performance.now() - start) / ms;
+				const n = a + (total - a) * easeInSine(r);
+				if (r >= 1 || Math.ceil(n) === total) {
+					return total;
+				} else {
+					raf.current = requestAnimationFrame(loop);
+					return Math.floor(n);
+				}
+			});
+		loop();
+	}, [total]);
 
 	return (
 		<>
 			<div className="flex items-baseline gap-2">
 				<span>you have</span>
-				<span className="font-serif text-3xl">${sum(cash, table)}</span>
+				<span className="font-serif text-3xl">${amount}</span>
 				{!table && <span>in hand</span>}
 			</div>
 			{table > 0 ? (
